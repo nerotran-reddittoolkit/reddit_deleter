a0905d670531ca09b9259f6329c4c5189dc45aa3
profnero <59196646+profnero@users.noreply.github.com>
2019-12-29 07:44:41 -0500

    Final version

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import date
 import time
 import authentication
 
@@ -9,7 +10,7 @@ user = reddit.user.me()
 
 # Get the current time from utc
 current_time = time.time()
-days = 100
+days = 0
 seconds = days * 3600 * 24
 
 # Run a loop through the user's comments and delete any whose posted date exceeds the days provided
@@ -18,7 +19,11 @@ for id in user.comments.new(limit=None):
     time = current_time - comment.created # Check if the datetime exceeds the limit
     if time > seconds:
         print("deleting comment: " + comment.id)
-        comment.delete() # delete the comment
+        with open("comments.csv", "a") as fa:
+            fa.writelines("reddit.com" + comment.submission.permalink + "\t"
+                         + comment.body.replace("\n", " ").encode("ascii", "replace").decode()
+                          + "\t" + str(date.fromtimestamp(comment.created)) + "\n")
+        #comment.delete() # delete the comment
 
 print()
 
@@ -27,6 +32,11 @@ for id in user.submissions.new(limit=None):
     post = reddit.submission(id=id) # initalize the post object using the id retrieved from the user
     time = current_time - post.created # Check if the datetime exceeds the limit
     if time > seconds:
-         print("deleting post: " + post.id)
-         post.delete() # delete the post
+        print("deleting post: " + post.id)
+        with open("posts.csv", "a") as fa:
+            fa.writelines("reddit.com/r/" + post.subreddit.display_name + "\t" + post.title
+                         + "\t" + str(date.fromtimestamp(post.created)) +
+                          "\t" + post.selftext.replace("\n", " ").encode("ascii", "replace").decode()
+                          + "\n")
+        #post.delete() # delete the post
 
