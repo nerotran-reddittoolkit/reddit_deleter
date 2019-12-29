84e17149022c53635838b280763a812869aabc0b
profnero <59196646+profnero@users.noreply.github.com>
2019-12-29 04:53:22 -0500

    separate authentication into another file

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
 import praw
+import authentication
 
-# Initialize a dictionary for hold the authentication info
-authentication = {
-    "client_id": "",
-    "client_secret": "",
-    "username": "",
-    "password": "",
-    "user_agent": ""
-}
+reddit = authentication.reddit
 
-# Read authentication info for OAuth2 from a text file for security reason
-with open("myauth.reddit", "r") as file_read:
-        for field in authentication:
-            authentication[field] = file_read.readline().strip("\n")
+print(reddit.read_only)
 
-#initialize a reddit object
-reddit = praw.Reddit(client_id=authentication["client_id"],
-                     client_secret=authentication["client_secret"],
-                     username=authentication["username"],
-                     password=authentication["password"],
-                     user_agent=authentication["user_agent"],)
